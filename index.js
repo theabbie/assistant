9ffc84902e4612fe0957218a043be0d3255a34fd
abhishek <abhishek7gg7@gmail.com>
2019-12-08 19:34:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var axios = require('axios');
 var app = express();
 app.use(express.json());
-function create(msg,card,ct,cs,cd,img,btn,url) {
+function create(msg,card,ct,cs,cd,img,btn,url,arr) {
 var result = {
   "fulfillmentText": msg,
   "payload": {
@@ -21,21 +21,6 @@ var result = {
   }
 };
 if (card) {
-result.fulfillmentMessages = [
-        {
-          "card": {
-            "title": ct,
-            "subtitle": cd,
-            "imageUri": img,
-            "buttons": [
-              {
-                "text": btn,
-                "postback": url
-              }
-            ]
-          }
-        }
-      ];
 result.payload.google.richResponse.items.push({
             "basicCard": {
               "title": ct,
@@ -55,6 +40,10 @@ result.payload.google.richResponse.items.push({
               "imageDisplayOptions": "CROPPED"
             }
           });
+if (arr) {
+result.payload.google.richResponse.suggestions = [];
+arr.forEach(function(x) {result.payload.google.richResponse.suggestions.push({"title": x})})
+}
 }
 return result;
 }
@@ -63,7 +52,7 @@ app.post("/*", async function(req,res) {
 //req.body.queryResult.queryText
 if (req.body.queryResult.queryText) {
 var data = (await axios("http://www.omdbapi.com/?t="+req.body.queryResult.queryText+"&apikey=2d58d444")).data;
-if (data.Title) {res.json(create("Movie Found",true,data.Title,data.Released,data.Plot,data.Poster,"More","https://google.com/search?q="+data.Title))}
+if (data.Title) {res.json(create("Movie Found",true,data.Title,data.Released,data.Plot,data.Poster,"More","https://google.com/search?q="+data.Title,["yes","no"]))}
 else {res.json(create("Movie Not Found",false))}
 }
 else {
