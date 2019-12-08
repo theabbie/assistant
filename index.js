a45d7fe8762b6c96a3e5f40aa5e1e9c89e6262a5
abhishek <abhishek7gg7@gmail.com>
2019-12-08 15:36:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(express.json());
 
 app.post("/*", function(req,res) {
+/*
 res.json({
 "fulfillmentText": req.body.queryResult.queryText.split(" ").reverse().join(" "),
 "google": {
@@ -18,6 +19,62 @@ res.json({
     }
   }
 })
+*/
+res.json(    {
+      "fulfillmentText": "This is a text response",
+      "fulfillmentMessages": [
+        {
+          "card": {
+            "title": "card title",
+            "subtitle": "card text",
+            "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+            "buttons": [
+              {
+                "text": "button text",
+                "postback": "https://assistant.google.com/"
+              }
+            ]
+          }
+        }
+      ],
+      "source": "example.com",
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "this is a simple response"
+                }
+              }
+            ]
+          }
+        },
+        "facebook": {
+          "text": "Hello, Facebook!"
+        },
+        "slack": {
+          "text": "This is a text response for Slack."
+        }
+      },
+      "outputContexts": [
+        {
+          "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+          "lifespanCount": 5,
+          "parameters": {
+            "param": "param value"
+          }
+        }
+      ],
+      "followupEventInput": {
+        "name": "event name",
+        "languageCode": "en-US",
+        "parameters": {
+          "param": "param value"
+        }
+      }
+    });
 })
 
 app.listen(process.env.PORT);
