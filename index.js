1fe3d80ef023ffa1774813b2fcdd42122452a8a1
abhishek <abhishek7gg7@gmail.com>
2019-12-08 12:54:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,44 +4,7 @@ app.use(express.json());
 
 app.post("/*", function(req,res) {
   res.json({
-      "fulfillmentText": "hello world",
-      "source": "https://theabbie.github.io",
-      "payload": {
-        "google": {
-          "expectUserResponse": true,
-          "richResponse": {
-            "items": [
-              {
-                "simpleResponse": {
-                  "textToSpeech": "hello world"
-                }
-              }
-            ]
-          }
-        },
-        "facebook": {
-          "text": "hello world"
-        },
-        "slack": {
-          "text": "hello world"
-        }
-      },
-      "outputContexts": [
-        {
-          "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-          "lifespanCount": 5,
-          "parameters": {
-            "param": "param value"
-          }
-        }
-      ],
-      "followupEventInput": {
-        "name": "event name",
-        "languageCode": "en-US",
-        "parameters": {
-          "param": "param value"
-        }
-      }
+      "fulfillmentText": "hello world"
    })
 })
 
