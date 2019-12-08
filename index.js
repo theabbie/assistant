6ab54c59ab10e5269990fa6e05d076fea913a4fd
abhishek <abhishek7gg7@gmail.com>
2019-12-08 18:57:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,9 @@ var express = require('express');
 var axios = require('axios');
 var app = express();
 app.use(express.json());
-
-app.post("/*", async function(req,res) {
-var magnet = "Welcome";
-if (req.body.queryResult.queryText) {
-magnet = (await axios("https://typi.tk/?url=https://thepiratebay.org/search/"+req.body.queryResult.queryText+"/0/0/1&sel=a[title='Download this torrent using magnet']&attribs=href&static=true")).data[0].attrib;
-}
-res.json({
-  "fulfillmentText": magnet,
-  "fulfillmentMessages": [
-        {
-          "card": {
-            "title": "Movie",
-            "subtitle": "Movie",
-            "imageUri": "https://theabbie.github.io/files/logo.png",
-            "buttons": [
-              {
-                "text": "Movie",
-                "postback": "https://theabbie.github.io"
-              }
-            ]
-          }
-        }
-      ],
+function create(msg,card,ct,cd,img,btn,url) {
+var result = {
+  "fulfillmentText": msg,
   "payload": {
     "google": {
       "expectUserResponse": true,
@@ -32,26 +12,10 @@ res.json({
         "items": [
           {
             "simpleResponse": {
-              "textToSpeech": magnet
+              "textToSpeech": msg
             }
-          },
-          {
-            "basicCard": {
-              "title": "Title: this is a title",
-              "subtitle": "This is a subtitle",
-              "formattedText": "This is a basic card.  Text in a basic card can include \"quotes\" and\n    most other unicode characters including emojis.  Basic cards also support\n    some markdown formatting like *emphasis* or _italics_, **strong** or\n    __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other\n    things like line  \nbreaks",
-              "image": {
-                "url": "https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png",
-                "accessibilityText": "Image alternate text"
-              },
-              "buttons": [
-                {
-                  "title": "This is a button",
-                  "openUrlAction": {
-                    "url": "https://assistant.google.com/"
-                  }
-                }
-              ],
+          }
+         ],
               "imageDisplayOptions": "CROPPED"
             }
           }
@@ -59,7 +23,46 @@ res.json({
       }
     }
   }
-});
+};
+if (card) {
+result.fulfillmentMessages = [
+        {
+          "card": {
+            "title": ct,
+            "subtitle": cd,
+            "imageUri": img,
+            "buttons": [
+              {
+                "text": btn,
+                "postback": url
+              }
+            ]
+          }
+        }
+      ];
+result.payload.google.richResponse.items.push({
+            "basicCard": {
+              "title": ct,
+              "subtitle": "From Chiri",
+              "formattedText": cd,
+              "image": {
+                "url": img,
+                "accessibilityText": ct
+              },
+              "buttons": [
+                {
+                  "title": btn,
+                  "openUrlAction": {
+                    "url": url
+                  }
+                });
+}
+return result;
+}
+
+app.post("/*", async function(req,res) {
+//req.body.queryResult.queryText
+res.json(create("Hello",false));
 })
 
 app.listen(process.env.PORT);
