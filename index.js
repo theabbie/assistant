4abaebcda94b93cddb7fc773fdeabccadc3ceaab
abhishek <abhishek7gg7@gmail.com>
2019-12-08 13:01:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,7 @@ app.use(express.json());
 
 app.post("/*", function(req,res) {
 res.json({
-"fulfillmentText": "hello world",
-"google": {
-    "expectUserResponse": true,
-    "richResponse": {
-      "items": [
-        {
-          "simpleResponse": {
-            "textToSpeech": "this is a simple response"
-          }
-        }
-      ]
-    }
-  }
+"fulfillmentText": req.body.queryResult.queryText
 })
 
 app.listen(process.env.PORT);
